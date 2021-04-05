8637dca66ab9bcc6a82611cb584b9262c83852f5
302698648@qq.com <leeneo@xingzhihen.com>
2021-04-05 13:41:43 +0800

    1

---
+++ ES6/ES6 箭头函数.js
@@ -0,0 +1,82 @@
+// 定义函数的几种方式
+
+// 1.使用 function 直接定义
+const lfn = function () {
+
+}
+
+// 2.对象的字面量中定义函数
+const obj = {
+    aaa: function () {
+
+    }, //==>等价于
+    bbb() {
+
+    }
+}
+
+// 3.ES6箭头函数
+// 无参
+const nopfn = () => {
+
+}
+// 多参
+const rfn = (pa, pb) => {
+    return pa + pb;
+}
+// 单参，可省略()
+const spfn = pa => {
+    return pa * 2;
+}
+// 内部单行代码，执行该行代码，并将结果返回
+const sfn = (pa, pb) => pa + pb
+console.log(sfn(4, 5));
+
+const clsfn = () => console.log('无参的单行代码演示');
+clsfn();
+
+
+// 箭头函数的使用
+
+// setTimeout(function () {
+//     console.log('定时器');
+// },200)
+setTimeout(() => {
+    console.log('es6定时器');
+}, 200)
+
+
+
+// 箭头函数中 this 的指向
+// 普通函数的this: 指向它的调用者,如果没有调用者则默认指向window.
+// 箭头函数的this: 指向箭头函数定义时所处的对象,而不是箭头函数使用时所在的对象,默认使用父级的this
+const objTest = {
+    names: {},
+    outFn() {
+        setTimeout(function () {
+            console.log('this: ', this); //指向的是 window(浏览器)或Timeout对象(node)
+
+            setTimeout(function () {
+                console.log('aa: this: ', this); //指向的是 window(浏览器)或Timeout对象(node)
+            }, 200) 
+            setTimeout(() => {
+                console.log('aa：箭头函数的this: ', this); //指向的是 window(浏览器)或Timeout对象(node)
+            }, 200)    
+        }, 200) 
+
+        setTimeout(() => {
+            console.log('箭头函数的this: ', this); //指向的是 objTest 对象
+
+            setTimeout(function () {
+                console.log('bb: this: ', this); //指向的是 window(浏览器)或Timeout对象(node)
+            }, 200) 
+            setTimeout(() => {
+                console.log('bb：箭头函数的this: ', this); //指向的是 objTest 对象
+            }, 200)    
+
+        }, 200)
+        // console.log(this);
+    }
+}
+objTest.outFn();
+// console.log(this);
---
+++ cli-demo/cli4-demo/cli4test/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/" tag="a" replace active-class="aactive">Home</router-link> |
+      <router-link to="/about" replace active-class="aactive">About</router-link> -->
+      <router-link to="/" tag="a" replace >Home</router-link> |
+      <router-link to="/about" replace >About</router-link>
+
+      <!-- <button @click="homeClick">首页</button>
+      <button @click="aboutClick">关于</button> -->
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+// export default {
+//   name: "app",
+//   methods: {
+//     // 通过代码的方式修改路由
+//     homeClick() {
+//       // this.$router.push("/");
+//       this.$router.replace("/");
+//       console.log("homeClick");
+//     },
+//     aboutClick() {
+//       // this.$router.push("about");
+//       this.$router.replace("about");
+//       console.log("aboutClick");
+//     },
+//   },
+// };
+</script>
