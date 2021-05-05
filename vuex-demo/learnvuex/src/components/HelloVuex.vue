ef229eaba91060bbf5e1aee578b083e7b177f282
302698648@qq.com <leeneo@xingzhihen.com>
2021-05-05 18:44:16 +0800

    1

--- vuex-demo/learnvuex/src/components/HelloVuex.vue
+++ vuex-demo/learnvuex/src/components/HelloVuex.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>    
+    <h1>{{ msg }}</h1>   
+    <h2>{{$store.state.counter}}</h2> 
+    <button @click='plus'>++</button>
+    <button @click="minus">--</button>
   </div>
 </template>
 
@@ -9,9 +12,18 @@ export default {
   name: 'HelloVuex',
   data () {
     return {
-      msg: 'HelloVuex'
+      msg: 'HelloVuex',
+    }
+  },
+  methods:{
+    plus(){
+      this.counter++;
+    },
+    minus(){
+      this.counter--;
     }
   }
+
 }
 </script>
 
