9675afa49ae2c0f03036b1429e46bfd1be234e95
302698648@qq.com <leeneo@xingzhihen.com>
2022-03-25 16:52:42 +0800

    learn vue 完成，store 新建分层

---
+++ ES6/对象解构.js
@@ -0,0 +1,11 @@
+const obj = {
+  nameA: 'lllll',
+  age: 32,
+  height: 2,
+  sex:'female'
+}
+
+// 可以不按原对象内的顺序
+const {nameA,height,age} = obj;
+
+console.log(nameA,height,age);
---
+++ vuex-demo/learnvuex/src/store/mutation-types.js
@@ -0,0 +1,3 @@
+// 多人协作时，方便管理
+export const INCREMENT = 'increment'
+export const DECREMENT = 'decrement'
