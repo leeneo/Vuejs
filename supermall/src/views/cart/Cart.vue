4f1d9b4e594d9ff98917f704e1e67737aded43c8
302698648@qq.com <leeneo@xingzhihen.com>
2022-04-22 12:01:41 +0800

    修改swiper

--- supermall/src/views/cart/Cart.vue
+++ supermall/src/views/cart/Cart.vue
@@ -1,11 +1,57 @@
 <template>
   <div class="cart">
-    <h2>购物车</h2>
+    <nav-bar class="cart-nav">
+      <div slot="center">购物车({{length}})</div>
+    </nav-bar>
+
+    <scroll class="wrapper">
+      <div class="content">
+        <cart-list></cart-list>
+      </div>
+    </scroll>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: "Cart",
-};
+  import NavBar from "components/common/navbar/NavBar";
+  import CartList from './childComps/CartList'
+  import Scroll from "components/common/scroll/Scroll";
+
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: "Cart",
+    components: {
+      NavBar,
+      CartList,
+      Scroll
+    },
+    computed: {
+      // cartLength() {
+      //   return this.$store.getters.cartLength;
+      // }
+      // getters 两种语法
+      // ...mapGetters(['cartLength']),
+      ...mapGetters({
+        length: 'cartLength',
+      })
+    }
+  };
 </script>
+
+<style scoped>
+  .cart-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+
+  .wrapper {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+  }
+</style>
---
+++ supermall/src/views/cart/childComps/CartList.vue
@@ -0,0 +1,37 @@
+<template lang="zh">
+    <div class="cart-list">
+        <!-- <scroll class="wrapper">
+            <div class="content">
+
+            </div>
+        </scroll> -->
+        <ul>
+            <li v-for="item in cartList">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    // import Scroll from "components/common/scroll/Scroll";
+
+    import { mapGetters } from 'vuex'
+
+    export default {
+        name: 'CartList',
+        components: {
+            // Scroll
+        },
+        computed: {
+            ...mapGetters(['cartList'])
+        }
+    }
+</script>
+<style lang="css" scoped>
+    /* .wrapper {
+        position: absolute;
+        top: 44px;
+        bottom: 49px;
+        left: 0;
+        right: 0;
+        overflow: hidden;
+    } */
+</style>
